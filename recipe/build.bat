fae33bd4ee52c1a59f1087cf109f4e06903eeee8
Daniel Bast <2790401+dbast@users.noreply.github.com>
2026-08-26 16:25:59 +0200

    Migrate recipe to v1, enable win-arm64 + linux-riscv64

---
+++ recipe/build.bat
@@ -0,0 +1,28 @@
+@echo on
+
+set CARGO_PROFILE_RELEASE_STRIP=symbols
+set CARGO_PROFILE_RELEASE_LTO=fat
+set CARGO_EXTRA_ARGS=
+if %target_platform% neq %build_platform% (
+  if %target_platform%==win-arm64 (
+    set CARGO_EXTRA_ARGS=--target aarch64-pc-windows-msvc
+  )
+  if %target_platform%==win-64 (
+    set CARGO_EXTRA_ARGS=--target x86_64-pc-windows-msvc
+  )
+)
+
+:: build
+cargo install --locked ^
+    --root "%PREFIX%" ^
+    --path . ^
+    --features all ^
+    --no-track ^
+    %CARGO_EXTRA_ARGS% ^
+    || exit 1
+
+:: dump licenses
+cargo-bundle-licenses ^
+    --format yaml ^
+    --output "%SRC_DIR%\THIRDPARTY.yml" ^
+    || exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,10 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
 
+if [[ "${target_platform}" == linux-* ]]; then
+    export CARGO_BUILD_RUSTFLAGS="${CARGO_BUILD_RUSTFLAGS:-} -C link-arg=${CONDA_BUILD_SYSROOT}/usr/lib64/libc_nonshared.a"
+fi
+
 cargo-bundle-licenses \
     --format yaml \
     --output THIRDPARTY.yml
